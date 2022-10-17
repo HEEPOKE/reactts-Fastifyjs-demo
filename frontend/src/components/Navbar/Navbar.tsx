ee75d6af576c0bd3c4cddb0d568d7158ca43fc13
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-17 12:48:32 +0700

    edit

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,30 +1,40 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { Space, Typography, Divider } from 'antd'
-import { If, Else, Then } from 'react-if'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Space, Typography, Divider } from "antd";
+import { If, Else, Then } from "react-if";
 
-import UserContext from '../../contexts/UserContext'
+import UserContext from "../../contexts/UserContext";
 
 const Navbar: React.FC = () => {
-  const { isLoggedIn } = useContext(UserContext)
+  const { isLoggedIn } = useContext(UserContext);
 
   return (
     <Space split={<Divider type="vertical" />}>
-      <Link to="/" component={Typography.Link}>Homepage</Link>
-      <Link to="/about" component={Typography.Link}>About</Link>
+      <Link to="/" component={Typography.Link}>
+        Homepage
+      </Link>
+      <Link to="/about" component={Typography.Link}>
+        About
+      </Link>
       <If condition={isLoggedIn}>
         <Then>
           <Space split={<Divider type="vertical" />}>
-            <Link to="/user/me" component={Typography.Link}>Me</Link>
-            <Link to="/logout" component={Typography.Link}>Logout</Link>
+            <Link to="/user/me" component={Typography.Link}>
+              Me
+            </Link>
+            <Link to="/logout" component={Typography.Link}>
+              Logout
+            </Link>
           </Space>
         </Then>
         <Else>
-          <Link to="/login" component={Typography.Link}>Login</Link>
+          <Link to="/login" component={Typography.Link}>
+            Login
+          </Link>
         </Else>
       </If>
     </Space>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
